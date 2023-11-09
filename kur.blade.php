c0a65b428957b73a26228abc1180408480465bab
1212falcon1212 <77229929+1212falcon1212@users.noreply.github.com>
2023-11-10 00:55:48 +0300

    Add files via upload

--- kur.blade.php
+++ kur.blade.php
@@ -1,1 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Convert USD to TRY</title>
+</head>
+<body>
+<h1>Convert USD to TRY</h1>
+<p>1 USD = {{ $rate }} TRY.</p>
+<p>1 Saatlik Ücret  = </p>
+</body>
+</html>
